7197e989ad16f03f6eba5f664f30894fe8295b1c
jasmine <jahliang@ucsc.edu>
2024-05-09 00:35:05 -0700

    improvements with collision and health

--- src/scenes/enemy.js
+++ src/scenes/enemy.js
@@ -3,34 +3,46 @@ class Enemy extends Phaser.Scene {
         super("enemyScene");
         this.my = {sprite: {}};
 
-        this.bodyX = 900;  // Initial position on the right side of the screen
+        this.bodyX = 900; // Initial position on the right side of the screen
         this.bodyY = 520;
-        this.stance = 'walk'; 
-        this.moveSpeed = 2;  // Speed of movement to the left
+        this.stance = 'walk';
+        this.moveSpeed = 2;
         this.counter = 0;
-        this.animationSpeed = 10; 
+        this.animationSpeed = 10;
         this.health = 1;
+        this.maxAppearances = 5;
+        this.healthText = null; // Text object for displaying health
     }
 
     preload() {
         this.load.setPath("./assets/");
-        this.load.image("enemy_jump", "spikeMan_jump.png"); 
+        this.load.image("enemy_jump", "spikeMan_jump.png");
         this.load.image("enemy_walking", "spikeMan_stand.png");
     }
-    
+
     create() {
-        let my = this.my;
-        // Create the sprite for walking and set it as the main body sprite
-        my.sprite.body = this.add.sprite(this.bodyX, this.bodyY, "enemy_walking");
-        // Create the sprite for jumping and set it as the alternate stance
-        my.sprite.stance = this.add.sprite(this.bodyX, this.bodyY, "enemy_jump");
-        my.sprite.stance.visible = false;  // Initially hide the jump sprite
+        this.my.sprite.body = this.physics.add.sprite(this.bodyX, this.bodyY, "enemy_walking");
+        this.my.sprite.stance = this.add.sprite(this.bodyX, this.bodyY, "enemy_jump");
+        this.my.sprite.stance.visible = false;
+
+        // Create health text display
+        this.healthText = this.add.text(this.my.sprite.body.x - 20, this.my.sprite.body.y - 30, 'Health: ' + this.health, {
+            fontSize: '16px',
+            fill: '#ffffff'
+        }).setOrigin(0.5, 0.5);
     }
 
     update() {
+        this.moveAndAnimate();
+
+        // Update the position of the health text to follow the enemy
+        this.healthText.x = this.my.sprite.body.x;
+        this.healthText.y = this.my.sprite.body.y - 30;
+    }
+
+    moveAndAnimate() {
         this.counter++;
 
-        // Toggle between stances
         if (this.counter % this.animationSpeed === 0) {
             if (this.stance === 'walk') {
                 this.my.sprite.stance.visible = true;
@@ -48,18 +60,36 @@ class Enemy extends Phaser.Scene {
         this.my.sprite.stance.x -= this.moveSpeed;
 
         // Reset position if it goes off screen to the left
-        if (this.my.sprite.body.x < -this.my.sprite.body.width) {  // Check if sprite moves off screen left
-            this.my.sprite.body.x = this.sys.game.config.width + this.my.sprite.body.width;  // Place it off-screen on the right
-            this.my.sprite.stance.x = this.sys.game.config.width + this.my.sprite.stance.width;
-        }
-    }
-    //collision detection
-    hitByCarrot(){
-        this.health -=1;
-        if (this.health <= 0) {
-            this.my.sprite.body.destroy(); // Or destroy if you prefer
-            this.my.sprite.visible = false;
+        if (this.my.sprite.body.x < -this.my.sprite.body.width) {
+            this.resetEnemyPosition();
         }
     }
 
+    resetEnemyPosition() {
+        if (this.appearanceCount < this.maxAppearances) {
+            this.my.sprite.body.x = this.sys.game.config.width + this.my.sprite.body.width;
+            this.my.sprite.stance.x = this.sys.game.config.width + this.my.sprite.stance.width;
+            this.appearanceCount++;
+        } else {
+            // Once maximum appearances are reached, stop spawning
+            this.my.sprite.body.setActive(false).setVisible(false);
+            this.my.sprite.stance.setActive(false).setVisible(false);
+            this.healthText.setVisible(false); // Hide health text
+        }
+    }
+
+    onCollisionWithRabbit(enemy, rabbit) {
+        this.decreaseHealth();
+        rabbit.scene.decreaseHealth(); 
+    }
+
+    decreaseHealth() {
+        this.health -= 1;
+        this.healthText.setText('Health: ' + this.health); // Update health display
+        if (this.health <= 0) {
+            this.my.sprite.body.destroy();
+            this.my.sprite.stance.destroy();
+            this.healthText.destroy(); // Destroy the health text display
+        }
+    }
 }
--- src/scenes/rabbit.js
+++ src/scenes/rabbit.js
@@ -1,61 +1,40 @@
 class Rabbit extends Phaser.Scene {
     constructor() {
         super("rabbitScene");
-        this.my = {sprite: {}};  // Create an object to hold sprite bindings
+        this.my = {sprite: {}};
 
-        // Create variables to hold constant values for sprite locations
         this.bodyX = 90;
         this.bodyY = 500;
-
-        //rabbit scene 
-        //rabbit sprite alternates assets when walking 
-        //can shoot carrots 
-        //has health points that decrease when interacting with enemy 
         this.stance = 'walk'; 
-        this.moveSpeed = 5;
-        //make array to manage carrots 
-        this.carrots = []
+        this.carrots = [];
         this.counter = 0;
-        this.animationSpeed = 10; 
-        this.health = 5;   
+        this.animationSpeed = 10;
+        this.health = 5;
     }
 
-    // Use preload to load art and sound assets before the scene starts running.
     preload() {
-        // Assets from Kenny Assets pack "Shape Characters"
-        // https://kenney.nl/assets/shape-characters
         this.load.setPath("./assets/");
-        // body
         this.load.image("walking", "bunny2_walk1.png");
-        this.load.image("normal", "bunny2_walk2.png"); 
-        this.load.image("carrot", "carrot.png"); 
-        // update instruction text
-        document.getElementById('description').innerHTML = '<h2>Gallery Shooter Game :D</h2>'
+        this.load.image("normal", "bunny2_walk2.png");
+        this.load.image("carrot", "carrot.png");
     }
 
     create() {
-        let my = this.my;   // create an alias to this.my for readability
+        this.my.sprite.body = this.physics.add.sprite(this.bodyX, this.bodyY, "walking");
+        this.my.sprite.stance = this.add.sprite(this.bodyX, this.bodyY, "normal");
+        this.my.sprite.stance.visible = false;
 
-        // Create the main body sprite
-        my.sprite.body = this.add.sprite(this.bodyX, this.bodyY, "walking");
-        my.sprite.stance = this.add.sprite(this.bodyX, this.bodyY, "normal"); 
-        my.sprite.stance.visible = false;
-        //shoot bullet on click
         this.spaceKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
-        
-         
+        this.healthText = this.add.text(10, 10, 'Health: ' + this.health, { fontSize: '20px', fill: '#fff' });
+
+        let enemyScene = this.scene.get('enemyScene');  // Assumed enemyScene is properly initialized and accessible
+        this.physics.add.collider(this.my.sprite.body, enemyScene.my.sprite.body, this.handleContact, null, this);
     }
 
     update() {
-        //my.sprite.walking.visible = true; 
-        // Since update is called multiple times/second, this.counter acts like
-        // a timer, increasing once per clock tick
-        //this.counter++;
-
-        //give the rabbit a running motion
         this.counter++;
 
-        if (this.counter % this.animationSpeed == 0) { 
+        if (this.counter % this.animationSpeed == 0) {
             if (this.stance === 'walk') {
                 this.my.sprite.stance.visible = true;
                 this.my.sprite.body.visible = false;
@@ -67,11 +46,33 @@ class Rabbit extends Phaser.Scene {
             }
         }
 
-        // Shooting carrots when space is pressed
         if (Phaser.Input.Keyboard.JustDown(this.spaceKey)) {
-            let carrot = this.physics.add.sprite(this.my.sprite.body.x, this.my.sprite.body.y, 'carrot');
-            carrot.setVelocityX(300); //move the carrot across the screen
-            this.carrots.push(carrot);  // Add the carrot to the carrots array for management
+            this.shootCarrot();
         }
     }
+
+    shootCarrot() {
+        let carrot = this.physics.add.sprite(this.my.sprite.body.x, this.my.sprite.body.y, 'carrot');
+        carrot.setVelocityX(300);
+        this.carrots.push(carrot);
+    }
+
+    handleContact() {
+        this.decreaseHealth();
+    }
+
+    decreaseHealth() {
+        this.health -= 1;
+        this.healthText.setText('Health: ' + this.health);
+        if (this.health <= 0) {
+            this.my.sprite.visible = false; // Hide the sprite
+            this.endGame(); // Call end game scenario
+        }
+    }
+
+    endGame() {
+        this.physics.pause(); // Stop physics to prevent further movement/collision
+        this.add.text(400, 300, 'Game Over', { fontSize: '40px', fill: '#fff' }).setOrigin(0.5);
+    }
 }
+ 
